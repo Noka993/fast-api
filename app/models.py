dbaeefbdacb75ed820972f96d03cc973edf3bbcb
Noka993 <Noka993@wp.pl>
2025-03-27 10:16:55 +0100

    Added voting system

--- app/models.py
+++ app/models.py
@@ -20,6 +20,7 @@ class Post(Base):
 
     owner = relationship("User")
 
+
 class User(Base):
     __tablename__ = "users"
 
@@ -28,4 +29,14 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
+
+
+class Vote(Base):
+    __tablename__ = "votes"
+    post_id = Column(
+        Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True
+    )
+    user_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
     )
